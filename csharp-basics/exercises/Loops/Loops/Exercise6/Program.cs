7bcb5d2f94b55b5b1c67dd5f249b111c87585cb9
TheBarbossa <sandis.blu@gmail.com>
2021-08-04 23:56:17 +0300

    Changed

--- csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
@@ -11,16 +11,14 @@ namespace Exercise6
             var max = Convert.ToInt32(Console.ReadLine());
             for (int i = 1; i <= max; i++)
             {
-                if (i % 3 == 0 || i % 5 == 0) {
-                    if (i % 3 == 0)
-                        Console.Write("Fizz");
-                    if (i % 5 == 0)
-                        Console.Write("Buzz");
-                }
-                else {
-                    Console.Write(i);
-                }
-                Console.Write(" ");
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.Write("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz");
+                else if (i % 5 == 0)
+                    Console.Write("Buzz");
+                else Console.Write(i);
+                    Console.Write(" ");
                 if (i % 20 == 0)
                     Console.Write("\n");
             }
