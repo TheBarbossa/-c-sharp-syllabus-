fdf256e21e514c9a7c35cd0b9402e50c81cc2a54
TheBarbossa <sandis.blu@gmail.com>
2021-08-25 12:55:47 +0300

    Net version changes

---
+++ csharp-basics/exercises/Polymorphism/DragRace/Ford.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragRace
+{
+    public class Ford : ICar
+    {
+        private int currentSpeed = 0;
+
+        public Ford()
+        {
+
+        }
+
+        public void SpeedUp()
+        {
+            currentSpeed+=5;
+        }
+
+        public void SlowDown()
+        {
+            currentSpeed+=5;
+        }
+
+        public string ShowCurrentSpeed()
+        {
+            return currentSpeed.ToString();
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Rrrrrrr.....");
+        }
+    }
+}
