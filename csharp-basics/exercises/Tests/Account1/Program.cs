d6bf0c661678096915d9394171ebcc28fc03e529
TheBarbossa <sandis.blu@gmail.com>
2021-09-06 13:47:47 +0300

    Inital exercise push

---
+++ csharp-basics/exercises/Tests/Account1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Account1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
